25dffad823e34a00e6e3df4456e7476541955c1c
zarezfamily <zarezfamilia@gmail.com>
2026-03-30 00:43:06 +0200

    Fix definitivo Error S - Interfaz Vivino

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,15 @@
-import { initializeApp } from "firebase/app";
+import * as FirebaseApp from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA1BcU0iRk3HyFtV92CLrnalHFKLaOWH24",
   authDomain: "miappdecafe.firebaseapp.com",
   projectId: "miappdecafe",
+  shadowProjectId: "miappdecafe", // Añadimos esto por si Hermes busca 'S' de Settings/Shadow
   storageBucket: "miappdecafe.firebasestorage.app",
   messagingSenderId: "274010206666",
   appId: "1:274010206666:web:d86abc7543e3772f8c9f33"
 };
 
-// Inicialización ultra-básica
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+// Inicialización directa sin constantes intermedias que Hermes pueda renombrar
+export const db = getFirestore(FirebaseApp.initializeApp(firebaseConfig));

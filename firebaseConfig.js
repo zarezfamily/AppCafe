987d67b27fccd4ceb1bdac71e9477557a8abe651
zarezfamily <zarezfamilia@gmail.com>
2026-03-29 16:35:11 +0200

    delete files

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,6 @@
 import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Tu configuración de Firebase (Extraída de tu repositorio)
 const firebaseConfig = {
   apiKey: "AIzaSyA1BcU0iRk3HyFtV92CLrnalHFKLaOWH24",
   authDomain: "miappdecafe.firebaseapp.com",
@@ -11,11 +10,8 @@ const firebaseConfig = {
   appId: "1:274010206666:web:d86abc7543e3772f8c9f33"
 };
 
-// Lógica de inicialización: 
-// Si la App ya está iniciada en memoria, la reutiliza. Si no, la crea.
+// Inicialización segura para Expo
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-
-// Exportamos la base de datos (Firestore) para usarla en App.js
 const db = getFirestore(app);
 
 export { db };

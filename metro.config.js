6a384a4dca7a61970ac4f7a85c5f09af7df960dd
zarezfamily <zarezfamilia@gmail.com>
2026-03-29 16:28:31 +0200

    cambios metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,8 @@
-cat <<EOF > metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Esta línea permite que Metro lea correctamente los módulos (.mjs) de Firebase
+// Esta línea es la que soluciona el error de la 'S' de Firebase
 config.resolver.sourceExts.push('mjs');
 
 module.exports = config;
-EOF

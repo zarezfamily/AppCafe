69799f09864989ac0947cac7f0d51020bde0db4e
zarezfamily <zarezfamilia@gmail.com>
2026-03-29 16:32:11 +0200

    delete scr/app

--- src/app/explore.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function ExploreScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
-      <Text style={{ fontSize: 18, fontWeight: 'bold' }}>Explore</Text>
-    </View>
-  );
-}
